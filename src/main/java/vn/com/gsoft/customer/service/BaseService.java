3a505c3bcf483e318e10fcf05286526c8d3ad1ef
vuductinh <vuductinh97@gmail.com>
2024-04-15 17:18:50 +0700

    thêm khôi phục khách hàng

--- src/main/java/vn/com/gsoft/customer/service/BaseService.java
+++ src/main/java/vn/com/gsoft/customer/service/BaseService.java
@@ -22,4 +22,7 @@ public interface BaseService<E,R, PK extends Serializable> {
 
     @Transactional(rollbackFor = {Exception.class, Throwable.class})
     boolean delete(PK id) throws Exception;
+
+    @Transactional(rollbackFor = {Exception.class, Throwable.class})
+    boolean restore(PK id) throws Exception;
 }
